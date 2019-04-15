19375d2a4d46587a04c51b4e9e0fa772a4769a3e
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-04-15 22:14:11 +0900

    Initial Commit

---
+++ shout_out/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shout_out
+//
+//  Created by 鈴木祥太 on 2019/04/15.
+//  Copyright © 2019 shiboy_aaashot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
