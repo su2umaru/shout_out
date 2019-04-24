cf4c695ba043d2641d176f9754088196daa473d5
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-04-24 23:59:03 +0900

    try to save and load data of words and members

--- shout_out/TitleController.swift
+++ shout_out/TitleController.swift
@@ -13,6 +13,8 @@ class TitleController: UIViewController {
     var wordArray: [String] = []
     var memberArray: [String] = []
     
+    var userDefaults = UserDefaults.standard
+    
     @IBOutlet weak var gameStartButton: UIButton!
     @IBOutlet weak var wordSettingButton: UIButton!
     @IBOutlet weak var memberSettingButton: UIButton!
@@ -20,17 +22,27 @@ class TitleController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let word1: String = "よっしゃ！"
-        let word2: String = "いぇーい！"
-        let word3: String = "いいね！"
-        self.wordArray.append(word1)
-        self.wordArray.append(word2)
-        self.wordArray.append(word3)
+//        self.userDefaults.removeObject(forKey: "words")
+//        self.userDefaults.removeObject(forKey: "members")
         
-        let member1: String = "たかし"
-        let member2: String = "よしこ"
-        self.memberArray.append(member1)
-        self.memberArray.append(member2)
+//        let word1: String = "よっしゃ！"
+//        let word2: String = "いぇーい！"
+//        let word3: String = "いいね！"
+//        self.wordArray.append(word1)
+//        self.wordArray.append(word2)
+//        self.wordArray.append(word3)
+//
+//        let member1: String = "たかし"
+//        let member2: String = "よしこ"
+//        self.memberArray.append(member1)
+//        self.memberArray.append(member2)
+        
+        // 起動時に保存データをロード
+        self.userDefaults.register(defaults: ["words": ["ワード1", "ワード2", "ワード3"], "members": ["メンバー1", "メンバー2", "メンバー3"]])
+        self.wordArray = self.userDefaults.array(forKey: "words") as! [String]
+        print(self.wordArray)
+        self.memberArray = self.userDefaults.array(forKey: "members") as! [String]
+        print(self.memberArray)
         
         self.gameStartButton.layer.borderWidth = 1.0
         self.gameStartButton.layer.borderColor = UIColor.white.cgColor
@@ -48,6 +60,13 @@ class TitleController: UIViewController {
     // Hide navigation bar
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+//        print("willappear loaded")
+        self.userDefaults.set(self.wordArray, forKey: "words")
+        print("words saved")
+        print(self.userDefaults.array(forKey: "words") as! [String])
+        self.userDefaults.set(self.memberArray, forKey: "members")
+        print("members saved")
+        print(self.userDefaults.array(forKey: "members") as! [String])
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
